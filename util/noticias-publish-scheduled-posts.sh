52658fd385dc94575c6f101a4f1968055af6b1ca
David Menache <dave@doap.com>
2015-03-04 16:47:38 +0000

    minor changes to laprensa.com.ni

--- util/noticias-publish-scheduled-posts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#mysql -u root -pfr1ck0ff -h db.doap.com noticias -e "UPDATE wp_2_posts SET post_status = 'publish' WHERE post_status = 'future' AND post_date < (CONVERT_TZ(NOW(),'America/Managua','UTC'));"
-mysql -u root -pfr1ck0ff -h db.doap.com noticias -e "UPDATE wp_2_posts SET post_status = 'publish' WHERE post_date_gmt > NOW() - INTERVAL 1 DAY and post_status = 'future' AND post_date_gmt < NOW();"
-
